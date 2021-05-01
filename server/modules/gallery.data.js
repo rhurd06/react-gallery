1c6cfac66e66454301842fe8615f7996912e6ba8
Robyn Noble <robynhurd09@gmail.com>
2021-05-01 08:44:36 -0500

    files set up and axios get written

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/Lamar-one-week-old.jpg', description: 'Photo of Lamar, roughly one week old.', likes: 0 },
+    { id: 3, path: 'images/Lamars-Baptism.jpg', description: 'Lamar and I at his baptism.', likes: 0 },
+    { id: 4, path: 'images/Vikings-with-Lamar.jpg', description: 'With my mini me in our jerserys.', likes: 0 },
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import GalleryList from '../Gallery List/GalleryList';
+
 
 function App() {
+  const [photoArray, setPhotoArray] = useState([]);
+
+  const getPhotos = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    })
+    .then(response => {
+      console.log('response from getPhotos', response.data);
+      setPhotoArray(response.data);
+    })
+    .catch(error => {
+      console.log('error in GET', error);
+    })
+  }
+
+  useEffect( () => {
+    setPhotoArray();
+  }, []);
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {/* <p>Gallery goes here</p>
+        <img src="images/goat_small.jpg"/> */}
+        <GalleryList />
       </div>
     );
 }
